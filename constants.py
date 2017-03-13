5d54ebfb38452cca57a3ecb2db1d69c593a2ee0a
Bart van de krol <Bart van de krol>
2017-03-13 12:19:10 +0100

    'bla'

---
+++ constants.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 13 12:07:14 2017
+
+@author: Bart
+"""
+
+global lambda1
+lambda1 = -0.0065         # temperature gradient in ISA [K/m]
+global p0
+p0      = 101325. #Pa
+global Temp0
+Temp0   = 288.15          # temperature at sea level in ISA [K]
+global R
+R       = 287.05          # specific gas constant [m^2/sec^2K]
+global g
+g       = 9.81 #m/s^2
+global gamma
+gamma   = 1.4
+global rho0
+rho0    = 1.225
+global W_s
+W_s     = 60500. #N
+global OEW
+OEW     = 9165. #lbs
+global m_fuel_0
+m_fuel_0 = 4150. #lbs
+global c
+c       = 2.0569	          # mean aerodynamic cord [m]
+
+global ft_to_m
+ft_to_m = 0.3048
+global kts_to_ms
+kts_to_ms = 0.514444
+global lbs_to_kg
+lbs_to_kg = 0.453592
