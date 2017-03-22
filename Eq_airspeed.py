41289a8d08aa3c4a8db3cb4beed89ccccbb98a21
Jakob the Almighty <vanlaarMJ@gmail.com>
2017-03-22 11:52:37 +0100

    Beep Boop ik ben een robot

--- Eq_airspeed.py
+++ Eq_airspeed.py
@@ -26,27 +26,5 @@ def equivalentspeed(h_p, TAT, V_IAS):
     V_t     =   M*a_mach
     V_e     =   V_t*np.sqrt(rho/rho0)
     
-    return V_e
-    
-def Truespeed(h_p, TAT, V_IAS):
-    TAT     =   TAT+273.15 #convert to Kelvin
-    V_IAS   =   V_IAS*kts_to_ms #convert to m/s
-    h_p     =   h_p*ft_to_m    
-    p       =   p0*(1.+(lambda1*h_p/Temp0))**(-g/(lambda1*R))
-    M       =   np.sqrt((2./(gamma-1.))*((((p0/p)*(((1.+(((gamma-1.)*rho0*V_IAS**2.)/(2*gamma*p0)))**(gamma/(gamma-1.)))-1.))+1.)**((gamma-1.)/gamma) -1.))
-    Temp    =   TAT/(1.+(((gamma-1.)/2)*M**2.))
-    a_mach  =   np.sqrt(gamma*R*Temp)
-    V_t     =   M*a_mach
-    
-    return V_t
-    
-def rhoeq(h_p, TAT, V_IAS):
-    TAT     =   TAT+273.15 #convert to Kelvin
-    V_IAS   =   V_IAS*kts_to_ms #convert to m/s
-    h_p     =   h_p*ft_to_m    
-    p       =   p0*(1.+(lambda1*h_p/Temp0))**(-g/(lambda1*R))
-    M       =   np.sqrt((2./(gamma-1.))*((((p0/p)*(((1.+(((gamma-1.)*rho0*V_IAS**2.)/(2*gamma*p0)))**(gamma/(gamma-1.)))-1.))+1.)**((gamma-1.)/gamma) -1.))
-    Temp    =   TAT/(1.+(((gamma-1.)/2)*M**2.))
-    rho     =   p/(R*Temp)
-    
-    return rho
+    return V_e, V_t, rho, M
+
